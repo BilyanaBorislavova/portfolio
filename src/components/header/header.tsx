001ccb8258cad3993f4c8283f49e9e0a036c3193
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-03 11:09:51 +0300

    Started implementing header sstyles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,12 @@
+import './header.scss';
+
 import * as React from 'react';
 import ViewMyWorkCta from '../cta/view-my-work-cta';
 
 const Header = () => (
     <header className="header">
         <p className="header-title">
-            Hello, I'm <span className="header-title-pink">Bilyana Borislavova</span>.
+            Hello, I'm <span className="header-title-highlighted">Bilyana Borislavova</span>.
             I'm a front end developer.
         </p>
         <ViewMyWorkCta
