76ee05476551fdcfeb0587d53c2df3c47a39a100
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-03 10:42:29 +0300

    Create view-my-work-cta.tsx

---
+++ src/components/cta/view-my-work-cta.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface IViewMyWorkCta {
+    onClick: () => void,
+    text: string
+}
+
+const ViewMyWorkCta = ({ onClick, text }: IViewMyWorkCta) => (
+    <button className="view-my-work-cta" onClick={onClick}>
+        {text}
+    </button>
+);
+
+export default ViewMyWorkCta;
