bfa1e5c39a8e1ff0a5a62713c98eb290c6571c6e
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-03 10:42:40 +0300

    Created header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import ViewMyWorkCta from '../cta/view-my-work-cta';
+
+const Header = () => (
+    <header className="header">
+        <p className="header-title">
+            Hello, I'm <span className="header-title-pink">Bilyana Borislavova</span>.
+            I'm a front end developer.
+        </p>
+        <ViewMyWorkCta
+          text="View my work"
+          onClick={() => console.log('here')}
+        />
+    </header>
+);
+
+export default Header;
