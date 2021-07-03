0e299e36e66caf3662b1b1930181a1d8dcd72a59
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-04 00:17:00 +0300

    Added Navigation to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './styles/styles.scss';
 
 import React from 'react';
 import Header from './components/header/header';
+import Navigation from './components/navigation/navigation';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navigation />
     </div>
   );
 }
