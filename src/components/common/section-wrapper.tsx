f4977d4582582ec740ae328802e4231552a59ea9
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-04 12:46:51 +0300

    Added custom className for SectionWrapper component

--- src/components/common/section-wrapper.tsx
+++ src/components/common/section-wrapper.tsx
@@ -1,19 +1,25 @@
 import './section-wrapper.scss';
 
 import React from 'react';
+import classNames from 'classnames';
 
 interface ISectionWrapper {
     id: string,
     title: string,
-    children: React.ReactNode
+    children: React.ReactNode,
+    className: string,
 }
-const SectionWrapper = ({ id, title, children }: ISectionWrapper) => (
-    <section className="section-wrapper" id={id}>
-        <h1 className="section-wrapper-title">
-            {title}
-        </h1>
-        {children}
-    </section>
-);
+const SectionWrapper = ({ id, title, children, className }: ISectionWrapper) => {
+    const combinedClassNames = classNames('section-wrapper', className);
+
+    return (
+        <section className={combinedClassNames} id={id}>
+            <h1 className="section-wrapper-title">
+                {title}
+            </h1>
+            {children}
+        </section>
+    );
+};
 
 export default SectionWrapper;
