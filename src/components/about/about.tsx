507a10eca0a7a2d38a0c261afc0474ac89b8139d
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-04 12:14:23 +0300

    Updated SectionWrapper to accept id

---
+++ src/components/about/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionWrapper from '../common/section-wrapper';
+import HexagonWithContent from '../hexagon-with-content/hexagon-with-content';
+
+const About = () => (
+    <SectionWrapper id="about" title="About">
+        <HexagonWithContent>
+            asd
+        </HexagonWithContent>
+    </SectionWrapper>
+);
+
+export default About;
--- src/components/common/section-wrapper.tsx
+++ src/components/common/section-wrapper.tsx
@@ -3,11 +3,12 @@ import './section-wrapper.scss';
 import React from 'react';
 
 interface ISectionWrapper {
+    id: string,
     title: string,
     children: React.ReactNode
 }
-const SectionWrapper = ({ title, children }: ISectionWrapper) => (
-    <section className="section-wrapper">
+const SectionWrapper = ({ id, title, children }: ISectionWrapper) => (
+    <section className="section-wrapper" id={id}>
         <h1 className="section-wrapper-title">
             {title}
         </h1>
