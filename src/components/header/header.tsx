c8eeb91548beba1dbbec94e5513bb40f37fb9b11
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-05 11:12:17 +0300

    Progress on header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,18 +2,25 @@ import './header.scss';
 
 import * as React from 'react';
 import ViewMyWorkCta from '../cta/view-my-work-cta';
+import { ROUTE_IDs } from '../../constants/constants';
 
-const Header = () => (
-    <header className="header">
-        <p className="header-title">
-            Hello, I'm <span className="header-title-highlighted">Bilyana Borislavova</span>.
-            I'm a front end developer.
-        </p>
-        <ViewMyWorkCta
-          text="View my work"
-          onClick={() => console.log('here')}
-        />
-    </header>
-);
+const { HEADER, ABOUT } = ROUTE_IDs;
+
+const Header = () => {
+    const aboutId = document.getElementById('about');
+    
+    return (
+        <header className="header" id={HEADER}>
+            <p className="header-title">
+                Hello, I'm <span className="header-title-highlighted">Bilyana Borislavova</span>.
+                I'm a front end developer.
+            </p>
+            <ViewMyWorkCta
+              text="View my work"
+              onClick={() => aboutId?.scrollIntoView({ behavior: 'smooth'})}
+            />
+        </header>
+    );
+};
 
 export default Header;
