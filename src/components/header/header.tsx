0c518f94353e1dae135a5a87e84872330fab3b9c
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-05 12:02:52 +0300

    Heaader and navigation fixes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,26 +1,20 @@
-import './header.scss';
+import "./header.scss";
 
-import * as React from 'react';
-import ViewMyWorkCta from '../cta/view-my-work-cta';
-import { ROUTE_IDs } from '../../constants/constants';
+import * as React from "react";
+import ViewMyWorkCta from "../cta/view-my-work-cta";
+import { ROUTE_IDs } from "../../constants/constants";
 
 const { HEADER, ABOUT } = ROUTE_IDs;
 
-const Header = () => {
-    const aboutId = document.getElementById('about');
-    
-    return (
-        <header className="header" id={HEADER}>
-            <p className="header-title">
-                Hello, I'm <span className="header-title-highlighted">Bilyana Borislavova</span>.
-                I'm a front end developer.
-            </p>
-            <ViewMyWorkCta
-              text="View my work"
-              onClick={() => aboutId?.scrollIntoView({ behavior: 'smooth'})}
-            />
-        </header>
-    );
-};
+const Header = () => (
+  <header className="header" id={HEADER}>
+    <p className="header-title">
+      Hello, I'm{" "}
+      <span className="header-title-highlighted">Bilyana Borislavova</span>. I'm
+      a front end developer.
+    </p>
+    <ViewMyWorkCta text="View my work" href={ABOUT} />
+  </header>
+);
 
 export default Header;
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,46 +1,44 @@
-import './navigation.scss';
+import "./navigation.scss";
 
-import React from 'react';
-import NavigationLink from './navigation-link';
-import { ROUTE_IDs } from '../../constants/constants'; 
+import React from "react";
+import NavigationLink from "./navigation-link";
+import { ROUTE_IDs } from "../../constants/constants";
 
 const { HEADER, ABOUT, PORTFOLIO, CONTACT } = ROUTE_IDs;
 
 const navigationLinks = [
-    {
-        id: 1,
-        text: 'Home',
-        href: HEADER,
-    },
-    {
-        id: 2,
-        text: 'About',
-        href: ABOUT,
-    },
-    {
-        id: 3,
-        text: 'Portfolio',
-        href: PORTFOLIO,
-    },
-    {
-        id: 4,
-        text: 'Contact',
-        href: CONTACT,
-    },
+  {
+    id: 1,
+    text: "Home",
+    href: HEADER,
+  },
+  {
+    id: 2,
+    text: "About",
+    href: ABOUT,
+  },
+  {
+    id: 3,
+    text: "Portfolio",
+    href: PORTFOLIO,
+  },
+  {
+    id: 4,
+    text: "Contact",
+    href: CONTACT,
+  },
 ];
 
 const Navigation = () => (
-    <nav className="navigation">
-        <ul className="navigation-list">
-            {
-                navigationLinks.map(({ id, text, href }) => (
-                    <li className="navigation-list-item" key={id}>
-                        <NavigationLink text={text} href={href} props={{ smooth: true }}/>
-                    </li>
-                ))
-            }
-        </ul>
-    </nav>
+  <nav className="navigation">
+    <ul className="navigation-list">
+      {navigationLinks.map(({ id, text, href }) => (
+        <li className="navigation-list-item" key={id}>
+          <NavigationLink text={text} href={href} />
+        </li>
+      ))}
+    </ul>
+  </nav>
 );
 
 export default Navigation;
