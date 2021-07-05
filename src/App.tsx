10fd403e01509fab47e26e25ab841822ccbd844c
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-05 11:13:11 +0300

    Added About to DOM

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './styles/styles.scss';
 import React from 'react';
 import Header from './components/header/header';
 import Navigation from './components/navigation/navigation';
+import About from './components/about/about';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navigation />
+      <About />
     </div>
   );
 }
