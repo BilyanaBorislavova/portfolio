1b3f2cce56fe24e39b00aad24f2f8fe980b1e279
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-05 17:32:08 +0300

    Added about me component with dummy  data

---
+++ src/components/about/about-me.tsx
@@ -0,0 +1,15 @@
+import './about-me.scss';
+
+import React from "react";
+
+const AboutMe = () => (
+  <section className="about-me">
+    <img alt="meeee" className="about-me-image" src="https://scontent-sof1-1.xx.fbcdn.net/v/t1.6435-9/185563256_10219137807983579_3903818128459165217_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=8Tus9PEpnWgAX9c8Wui&tn=i91jZkdHUPsjrRcm&_nc_ht=scontent-sof1-1.xx&oh=4916a5e9710c90c543be57b58a49c2bf&oe=60E7194D" />
+    <h4 className="about-me-title">Who am I?</h4>
+    <p className="about-me-description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit veniam dignissimos esse officia recusandae corrupti provident enim error! Voluptatem totam hic et est voluptates cumque soluta necessitatibus ducimus fuga iure.
+    </p>
+  </section>
+);
+
+export default AboutMe;
