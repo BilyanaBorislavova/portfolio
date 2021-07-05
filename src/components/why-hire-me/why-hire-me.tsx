d1e8c45cb86bc26b7b2e38923f0c1b511fcb3dae
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-05 11:09:40 +0300

    Added WhyHireMe components

---
+++ src/components/why-hire-me/why-hire-me.tsx
@@ -0,0 +1,35 @@
+import './why-hire-me.scss';
+
+import React from 'react';
+import HexagonWithContentAndText from '../hexagon/hexagon-with-content-and-text';
+
+const WhyHireMe = () => (
+  <section className="why-hire-me">
+    <HexagonWithContentAndText
+      title="Fast"
+      text="Fast load times and lag free interaction, my highest priority."
+    >
+      asd
+    </HexagonWithContentAndText>
+    <HexagonWithContentAndText
+      title="Fast"
+      text="Fast load times and lag free interaction, my highest priority."
+    >
+      asd
+    </HexagonWithContentAndText>
+    <HexagonWithContentAndText
+      title="Fast"
+      text="Fast load times and lag free interaction, my highest priority."
+    >
+      asd
+    </HexagonWithContentAndText>
+    <HexagonWithContentAndText
+      title="Fast"
+      text="Fast load times and lag free interaction, my highest priority."
+    >
+      asd
+    </HexagonWithContentAndText>
+  </section>
+);
+
+export default WhyHireMe;
