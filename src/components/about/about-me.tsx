a998fef451db2402a812d8bd58eb5b595b8e65dd
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-07-13 15:13:41 +0300

    Changed about me picture and added more skills to skills section

--- src/components/about/about-me.tsx
+++ src/components/about/about-me.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const AboutMe = () => (
   <section className="about-me">
-    <img alt="meeee" className="about-me-image" src="https://scontent-sof1-1.xx.fbcdn.net/v/t1.6435-9/185563256_10219137807983579_3903818128459165217_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=8Tus9PEpnWgAX9c8Wui&tn=i91jZkdHUPsjrRcm&_nc_ht=scontent-sof1-1.xx&oh=4916a5e9710c90c543be57b58a49c2bf&oe=60E7194D" />
+    <img alt="meeee" className="about-me-image" src="https://scontent.fsof9-1.fna.fbcdn.net/v/t1.6435-9/213489339_10219445962647253_5083414308859747979_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=12c3Y-vuD1cAX8H7tBa&_nc_ht=scontent.fsof9-1.fna&oh=ff4433cda7a97841264c85ef0a9668d6&oe=60F34FE5" />
     <h4 className="about-me-title">Who am I?</h4>
     <p className="about-me-description">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit veniam dignissimos esse officia recusandae corrupti provident enim error! Voluptatem totam hic et est voluptates cumque soluta necessitatibus ducimus fuga iure.
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,17 +1,58 @@
 import React from "react";
 import SkillWithProgressBar from "./skill-with-progress-bar";
 
+const commonPropsForSkills = {
+  strokeWidth: 10.1,
+  trailWidth: 10.1,
+  strokeLinecap: "",
+};
+
+const skills = [
+  {
+    id: "JavaScript",
+    text: "JavaScript",
+    percentageOfKnowledge: 70,
+    props: commonPropsForSkills,
+  },
+  {
+    id: "React",
+    text: "React",
+    percentageOfKnowledge: 80,
+    props: commonPropsForSkills,
+  },
+  {
+    id: "Express",
+    text: "Express",
+    percentageOfKnowledge: 45,
+    props: commonPropsForSkills,
+  },
+  {
+    id: "HTML & CSS/SASS/LESS",
+    text: "HTML & CSS/SASS/LESS",
+    percentageOfKnowledge: 90,
+    props: commonPropsForSkills,
+  },
+  {
+    id: "Jquery",
+    text: "Jquery",
+    percentageOfKnowledge: 65,
+    props: commonPropsForSkills,
+  },
+];
+
 const Skills = () => (
   <section className="skills">
-    <SkillWithProgressBar
-      skillText="JavaScript"
-      percentageOfKnowledge={70}
-      props={{
-          strokeWidth: 10.1,
-          trailWidth: 10.1,
-          strokeLinecap: ''
-    }}
-    />
+    <ul className="skills-list">
+      {skills.map(({ id, text, percentageOfKnowledge, props }) => (
+        <li className="skills-list-item" key={id}>
+          <SkillWithProgressBar
+            skillText={text}
+            percentageOfKnowledge={percentageOfKnowledge}
+            props={props}
+          />
+        </li>
+      ))}
+    </ul>
   </section>
 );
 
